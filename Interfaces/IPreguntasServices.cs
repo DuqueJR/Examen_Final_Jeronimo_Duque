5a07c28d0bf8d12a8e2634b4d07ba893495e8a17
DuqueJR <duqueruizjeronimo@gmail.com>
2026-05-25 08:53:31 -0500

    feat(DTOs): Creacion de DTO para creaciond de pregunta

---
+++ Interfaces/IPreguntasServices.cs
@@ -0,0 +1,6 @@
+﻿namespace FINAL_WEB_JERONIMO_DUQUE_RUIZ.Interfaces
+{
+    public interface IPreguntasServices
+    {
+    }
+}
---
+++ Models/DTOs/CreatePreguntaDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FINAL_WEB_JERONIMO_DUQUE_RUIZ.Models.DTOs
+{
+    public class CreatePreguntaDTO
+    {
+        [Required]
+        [MinLength(8)]
+        public string Enunciado { get; set; }
+    }
+}
