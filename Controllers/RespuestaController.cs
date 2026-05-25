23e5746e93a6f26fac112fc17bb278fb19d4dd8c
DuqueJR <duqueruizjeronimo@gmail.com>
2026-05-25 09:46:09 -0500

    feat(Controller): Controller para respuesta

---
+++ Controllers/RespuestaController.cs
@@ -0,0 +1,34 @@
+﻿using FINAL_WEB_JERONIMO_DUQUE_RUIZ.Interfaces;
+using FINAL_WEB_JERONIMO_DUQUE_RUIZ.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FINAL_WEB_JERONIMO_DUQUE_RUIZ.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class RespuestaController : Controller
+    {
+        private readonly IRespuestaService _service;
+
+        public RespuestaController (IRespuestaService service)
+        {
+            _service = service;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody]CreatedRespuestaDTO dto)
+        {
+            var created = await _service.Create(dto);
+
+            return created == null ? NotFound("Pregutna no encontrada o no disponible para responder") : Ok(created);
+
+        }
+    }
+}
