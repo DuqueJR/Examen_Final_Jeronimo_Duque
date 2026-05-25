d1003b57b4d97ceaa1a98a76934743caa269ef55
DuqueJR <duqueruizjeronimo@gmail.com>
2026-05-25 09:35:42 -0500

    feat(Models): Crear modelo de respuesta

---
+++ Models/Respuesta.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FINAL_WEB_JERONIMO_DUQUE_RUIZ.Models
+{
+    public class Respuesta
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid id { get; set; }
+
+        [MinLength(2)]
+        public string enunciado { get; set; }
+
+        public Guid pregunta_Id { get; set; }
+
+        [ForeignKey(nameof(pregunta_Id))]
+        public Preguntas pregunta { get; set; } = null!;
+
+    }
+}
